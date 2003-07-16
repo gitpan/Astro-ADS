951bcbc2f529827f55ff396a5ceec719d3a2debd
Alasdair Allan <aa@astro.ex.ac.uk>
2003-07-16 18:59:04 +0000

    Import of AALLAN/Astro-ADS-1.20.7 from CPAN.
    gitpan-cpan-distribution: Astro-ADS
    gitpan-cpan-version:      1.20.7
    gitpan-cpan-path:         AALLAN/Astro-ADS-1.20.7.tar.gz
    gitpan-cpan-author:       AALLAN
    gitpan-cpan-maturity:     released

---
+++ ADS.pm
@@ -0,0 +1,40 @@
+package Astro::ADS;
+
+=head1 NAME
+
+Astro::ADS - An object orientated interface to NASA's ADS database
+
+=head1 DESCRIPTION
+
+This module does nothing, and is here as a placeholder in case of need. The
+user interface to the goodness wrapped in the ADS package can be found in
+the L<Astro::ADS::Query> module.
+
+=head1 REVISION
+
+$Id: ADS.pm,v 1.2 2003/07/16 18:56:43 aa Exp $
+
+=head1 METHODS
+
+The Astro::ADS module itself has no methods, and is just a placeholder
+module, see L<Astro::ADS::Query> for the actual interface.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001-2003 University of Exeter. All Rights Reserved.
+
+This program was written as part of the eSTAR project and is free software;
+you can redistribute it and/or modify it under the terms of the GNU Public
+License.
+
+=head1 AUTHORS
+
+Alasdair Allan E<lt>aa@astro.ex.ac.ukE<gt>,
+
+=cut
+
+use strict;
+use vars qw/ $VERSION /;
+$VERSION = '1.20.7'
+
+1;                                                                  
